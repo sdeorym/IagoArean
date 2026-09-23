3127ceaaea2673f232ed6d92b4e4bf0930c2db94
Susana <hornosu@yahoo.es>
2026-09-23 18:30:19 +0200

    feat: contact; falta que se pueda enviar

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,12 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const url = require('url');
+import cors from "cors";
+import dotenv from "dotenv";
+import { Resend } from 'resend';
+
+dotenv.config();
+
 import { fileURLToPath } from "url";
 import express from "express";
 
@@ -26,3 +32,39 @@ app.get('/', (req, res) => {
     res.status(200), JSON(data);
 });
 
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+var access = fs.createWriteStream('./node.access.log', { flags: 'a' });
+//var error = fs.createWriteStream('./node.error.log', { flags: 'a' });
+
+process.stdout.write = process.stderr.write = access.write.bind(access);
+
+dotenv.config();
+
+app.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', '/index.html'));
+});
+
+
+app.post("/contact", async (req, res) => {
+  const url= process.env.MAIL_URL;
+  const API_key = process.env.RESEND_API_KEY;
+  // const basicAuth = btoa(pw);
+
+  const { username, email, message } = req.body;
+
+  const resend = new Resend(API_key);
+
+  const { data, error } = await resend.emails.send({
+    from: 'info@lpgconsulting.fr',
+    to: ['sdeorym@gmail.com'],
+    subject: '[Webpage contact]',
+    text: 'From: ' + username + ' Email: ' + email + ' Message: ' + message
+    });
+  res.status(200).json({ success: true });
+});
+  
+
+app.listen(process.env.PORT || 3000, "0.0.0.0")
--- Frontend/src/pages/Contact.jsx
+++ Frontend/src/pages/Contact.jsx
@@ -1,35 +1,71 @@
 import contact from '@assets/contact.jpg'
 import '@styles/Contact.css';
 import Button from "@components/Button";
+import { useState } from "react";
 /*= {((formData.username!="") && (formData.email!="")) ? "submit enabled" : "submit disabled"}*/ "submit" 
           /*disabled = {((formData.username=="") || (formData.email=="")) ? true : false}
           data={formData}*/
 
 function Contact() {
+  const [formData, setFormData] = useState({username: '', email: '', message: ''});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value});
+  };
+
+  const handleSubmit = async (e) => {
+      e.preventDefault();
+      const dataToSend = {username: formData.username, email: formData.email, message: formData.message};
+      const stringifiedJsonData = JSON.stringify(dataToSend);
+      try {
+          const response = await fetch("/contact", {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: stringifiedJsonData
+          });
+          
+          if (!response.ok) {
+              const text = await response.text();
+              throw new Error("ERROR. Please, try later.");
+          }
+          const result = await response.json();
+          alert("Your message has been sent");
+          setFormData({username: '', email: '', message: ''});
+          e.target.reset();
+      }
+      catch(error) {
+          alert("ERROR. Please, try later.");
+      }
+  }
 
   return (
     <section id="contact">
-      <img src={contact} alt="autorretratos de Iago Arean"></img>
+      <img src={contact} alt="Iago Arean's self-portrait in Egon Schiele style."></img>
       <form className="contactForm">
         <div className="textbox">
           <label htmlFor="username">Nom</label>
-          <input placeholder="Votre nom ici" type="text" id="username" name="username" autoComplete="name" /*onBlur={handleChange}*/ required />
+          <input placeholder="Your name here" type="text" id="username" name="username" autoComplete="name" onBlur={handleChange} required />
         </div>
         <div className="textbox">
           <label htmlFor="email">Courriel</label>
-          <input placeholder="info@example.com" type="email" id="email" name="email" autoComplete="email" /*onBlur={handleChange}*/ required />
+          <input placeholder="info@example.com" type="email" id="email" name="email" autoComplete="email" onBlur={handleChange} required />
         </div>
         <div className="textbox">
           <label htmlFor="message">Message</label>
-          <textarea id="message" className="messagebox" type="message" name="message" rows="10" placeholder="Votre message ici" /*onBlur={handleChange}*/ required />
+          <textarea id="message" className="messagebox" type="message" name="message" rows="10" placeholder="Your message here" onBlur={handleChange} required />
         </div>
         <Button 
           type="submit" 
           value="Send" 
           title="Envoyer" 
-          text="Envoyer"
-          classname = "submit"
-          aria="Bouton envoyer message"     
+          text="Envoyer" 
+          classname = {((formData.username!="") && (formData.email!="")) ? "submit enabled" : "submit disabled"}
+          disabled = {((formData.username=="") || (formData.email=="")) ? true : false}
+          data={formData} 
+          aria="Button to send message"     
         />
       </form>
     </section>
